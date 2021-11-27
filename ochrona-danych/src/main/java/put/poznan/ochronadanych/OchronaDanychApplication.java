4e9474851dee82311e8747cfeb073044da8c8c52
konrad <konradwodnicki98@gmail.com>
2021-11-27 11:54:30 +0100

    add spring boot project

---
+++ ochrona-danych/src/main/java/put/poznan/ochronadanych/OchronaDanychApplication.java
@@ -0,0 +1,13 @@
+package put.poznan.ochronadanych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OchronaDanychApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OchronaDanychApplication.class, args);
+	}
+
+}
---
+++ ochrona-danych/src/test/java/put/poznan/ochronadanych/OchronaDanychApplicationTests.java
@@ -0,0 +1,13 @@
+package put.poznan.ochronadanych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OchronaDanychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
